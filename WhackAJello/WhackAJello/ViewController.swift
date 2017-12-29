adede03261e9cdbfec8b186825c88d6a5811e374
Christopher Lee <hanernlee@gmail.com>
2017-12-30 01:24:23 +1100

    3D Model

--- WhackAJello/WhackAJello/ViewController.swift
+++ WhackAJello/WhackAJello/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        sceneView.session.run(configuration)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func play(_ sender: UIButton) {
+        addNode()
     }
-
-
+    
+    @IBAction func reset(_ sender: UIButton) {
+    }
+    
+    func addNode() {
+        let jellyfishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyfishNode = jellyfishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        jellyfishNode?.position = SCNVector3(0, 0, -1)
+        sceneView.scene.rootNode.addChildNode(jellyfishNode!)
+        
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! SCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        
+        if hitTest.isEmpty {
+            print("Didn't touch any objects")
+        } else {
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            
+            
+        }
+    }
 }
 
